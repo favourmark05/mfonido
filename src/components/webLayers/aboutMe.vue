f506cbcf7b338be02bd48ba813083e155f8d8351
MFONIDO MARK <49275804+favourmark05@users.noreply.github.com>
2021-10-15 23:10:36 +0100

    Add files via upload

---
+++ src/components/webLayers/aboutMe.vue
@@ -0,0 +1,68 @@
+<template>
+  <div>
+      <main>
+          <section class="section-about">
+              <div class="center-head">
+                  <h2 class="heading">
+                  About Me
+              </h2>
+              </div>
+              <div class="contain">
+                  <div class="col-1">
+                      Text here
+                  </div>
+                  <div class="col-2">
+                      text here also
+                  </div>
+              </div>
+          </section>
+      </main>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+.section-about{
+    background-color: #f7f7f7;
+    padding: 12rem 0;
+    margin-top: -20vh;
+}
+.center-head{
+    text-align: center;
+}
+.heading{
+    font-size: 3rem;
+    font-weight: 700;
+    background-image: linear-gradient(to right,rgba(189, 61, 61, 0.575),
+      rgba(255, 0, 0, 0.842));
+      display: inline-block;
+      -webkit-background-clip: text;
+      color: transparent;
+      text-shadow: 0 7px 80px rgba(58, 55, 55, 0.589), 0 3px 5px rgba(58, 55, 55, 0.589);
+      animation-name: moveInRight;
+      animation-duration: 3s;
+      letter-spacing: 1rem;
+}
+.heading:hover{
+    transform: skewY(2deg) skewX(15deg)
+}
+.contain{
+    display: flex;
+    justify-content: space-around;
+}
+@keyframes moveInRight {
+  0%{
+    opacity: 0;
+    transform: translateX(100px);
+  }
+  100%{
+    opacity: 1;
+    transform: translate(0);
+  }
+}
+</style>
