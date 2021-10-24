460a36713c714a3a22299f52ec6b0ada053fc48e
Mfonido Mark <mfonidomark@localhost.localdomain>
2021-10-24 11:31:50 +0100

    contact form

--- src/components/webLayers/contact.vue
+++ src/components/webLayers/contact.vue
@@ -1,28 +1,91 @@
 <template>
   <div>
-      <div class="heading">
-              <h1>CONTACT ME</h1>
-          </div>
-      <section class="contact-me">
-              <div class="form">
-              <div class="col-1">
-                  <form action="">
-                      <h2>Say Hi!</h2>
-                      <input type="text" placeholder="Full Name" required>
-                      <input type="email" name="email" id="" placeholder="Email" class="email" required>
-                      <textarea name="message" id="" cols="30" rows="10" placeholder="Enter your message here" required></textarea> <br>
-                      <button>Send Mail</button>
-                  </form>
-              </div>
-              <div class="col-2"></div>
-          </div>
-      </section>
+    <div class="heading">
+      <h1>CONTACT ME</h1>
+    </div>
+    <section class="contact-me">
+      <div class="form">
+        <div class="col-1">
+          <form @submit.prevent="sendMail">
+            <h2>Say Hi!</h2>
+            <input
+              type="text"
+              placeholder="Full Name"
+              v-model="fullName"
+            />
+            <input
+              type="email"
+              name="email"
+              id=""
+              placeholder="Email"
+              class="email"
+              v-model="email"
+            />
+            <textarea
+              name="message"
+              id=""
+              cols="30"
+              rows="10"
+              placeholder="Enter your message here"
+              v-model="message"
+            ></textarea>
+            <br />
+            <button type="submit" value="send" v-bind:disabled="!message  || !fullName || !email">Send Mail</button>
+          </form>
+        </div>
+        <div class="col-2"></div>
+      </div>
+    </section>
   </div>
 </template>
 
 <script>
+import emailjs from 'emailjs-com'
 export default {
-
+  name: 'ContactMe',
+  data () {
+    return {
+      fullName: '',
+      email: '',
+      message: ''
+    }
+  },
+  methods: {
+    sendMail (e) {
+      try {
+        emailjs
+          .sendForm(
+            'service_41yaaok',
+            'template_fszzds4',
+            e.target,
+            'user_JnkbPpytmFMhAyAWo3zVs',
+            {
+              fullName: this.fullName,
+              email: this.email,
+              message: this.message
+            }
+          )
+          .then(() => {
+            this.$swal.fire(
+              'Message Sent!',
+              'Will get back to you shortly!',
+              'success'
+            )
+          })
+      } catch (error) {
+        console.log({ error })
+      }
+      // Reset form field
+      this.fullName = ''
+      this.email = ''
+      this.message = ''
+    }
+  },
+  computed: {
+    isDisabled () {
+      return this.message.length < 10 && this.fullName === '' && this.email === ''
+    }
+  }
 }
 </script>
 
@@ -31,10 +94,11 @@ export default {
   font-size: 1rem;
   font-weight: 700;
   /* color: white; */
-   background-image: linear-gradient(
+  background-image: linear-gradient(
     to right,
     rgba(224, 17, 17, 0.575),
-    rgb(255, 0, 0));
+    rgb(255, 0, 0)
+  );
   -webkit-background-clip: text;
   color: transparent;
   text-shadow: 0 2rem 5rem rgba(0, 0, 0, 0.555);
@@ -56,80 +120,83 @@ export default {
     transform: translate(0);
   }
 }
-.contact-me{
-    background: linear-gradient(
-      to right,
-      rgba(189, 61, 61, 0.575),
-      rgba(206, 79, 79, 0.664)
-    );
-    height: 100%;
-    display: flex;
-    padding: 20px;
+.contact-me {
+  background: linear-gradient(
+    to right,
+    rgba(189, 61, 61, 0.575),
+    rgba(206, 79, 79, 0.664)
+  );
+  height: 100%;
+  display: flex;
+  padding: 20px;
 }
-.form{
-    background-image:linear-gradient(105deg,
-    rgba(255, 255, 255, .9)0%,
-    rgba(255, 255, 255, .9)50%,
-    transparent 50%),
+.form {
+  background-image: linear-gradient(
+      105deg,
+      rgba(255, 255, 255, 0.9) 0%,
+      rgba(255, 255, 255, 0.9) 50%,
+      transparent 50%
+    ),
     url("../../assets/contact.jpg");
-    background-size: cover;
-    background-position: center;
-     margin: auto;
-     display: grid;
-     grid-template-columns: 50% 50%;
-     width: 55vw;
-     text-align: center;
-     border-radius: 1em;
-     box-shadow: 0 1.5rem 4rem rgba(0, 0, 0, 0.726);
+  background-size: cover;
+  background-position: center;
+  margin: auto;
+  display: grid;
+  grid-template-columns: 50% 50%;
+  width: 55vw;
+  text-align: center;
+  border-radius: 1em;
+  box-shadow: 0 1.5rem 4rem rgba(0, 0, 0, 0.726);
 }
-button{
-    margin: 5px;
-    cursor: pointer;
+button {
+  margin: 5px;
+  cursor: pointer;
 }
-button:hover{
-    background-color: rgba(206, 79, 79, 0.795);
-    color: white;
-    border-radius: 1em;
-    cursor: pointer;
+button:hover {
+  background-color: rgba(206, 79, 79, 0.795);
+  color: white;
+  border-radius: 1em;
+  cursor: pointer;
 }
-input{
-    margin: 10px;
+input {
+  margin: 10px;
 }
-textarea{
-    margin-top: 10px;
+textarea {
+  margin-top: 10px;
 }
-.label-name, .label-email{
-    display: block;
-    text-align: left;
-    margin-left: 70px;
+.label-name,
+.label-email {
+  display: block;
+  text-align: left;
+  margin-left: 70px;
 }
-input:focus{
-    outline: none;
-    box-shadow: 0 1rem 2rem black;
-    border-bottom: 3px solid rgba(206, 79, 79, 0.795);
-    padding: 7px;
-    border-radius: .5em;
+input:focus {
+  outline: none;
+  box-shadow: 0 1rem 2rem black;
+  border-bottom: 3px solid rgba(206, 79, 79, 0.795);
+  padding: 7px;
+  border-radius: 0.5em;
 }
-input:focus:invalid{
-    border-bottom: 3px solid rgba(255, 238, 0, 0.795);
+input:focus:invalid {
+  border-bottom: 3px solid rgba(255, 238, 0, 0.795);
 }
 form > span {
-    color: rgb(0, 0, 0);
+  color: rgb(0, 0, 0);
 }
 
-.col-1{
-    border-bottom-left-radius: 1em;
-    border-top-left-radius: 1em;
+.col-1 {
+  border-bottom-left-radius: 1em;
+  border-top-left-radius: 1em;
 }
-@media only screen and (max-width: 578px){
-    .form{
-        grid-template-columns: auto;
-    }
-    .col-2{
-        display: none;
-    }
-    .col-1{
-        border-radius: 1em;
-    }
+@media only screen and (max-width: 578px) {
+  .form {
+    grid-template-columns: auto;
+  }
+  .col-2 {
+    display: none;
+  }
+  .col-1 {
+    border-radius: 1em;
+  }
 }
 </style>
