ea606e4d04a0b7c1046f638917924a79ffa57817
Mfonido Mark <favourmark05@gmail.com>
2025-03-16 20:24:25 +0100

    feat(projects): update project descriptions and add new projects to the API

--- src/Api/projects.js
+++ src/Api/projects.js
@@ -7,9 +7,7 @@ export const projects = [{
 image: thePixploreImg,
 url:'https://thepixplore.netlify.app/',
 title: 'PixPlore',
-description: `Web app for visualizing personalized Spotify data. View your top artists, top tracks,
-      recently played tracks, and detailed audio information about each track. Create and save
-      new playlists of recommended tracks based on your existing playlists and more.`,
+description: `A web app for searching and downloading high-quality images using the Unsplash API. Users can search for any image and explore a vast collection of stunning visuals. The app provides an intuitive interface to browse images, view details, and download the best available version with ease.`,
 technology: [
   'vue3',
   'scss',
@@ -26,9 +24,7 @@ linkText: 'thepixplore.netlify.app'
   image: developerImg,
   url:'https://developers.remita.net/',
   title: 'Developer Page',
-  description: `Web app for visualizing personalized Spotify data. View your top artists, top tracks,
-            recently played tracks, and detailed audio information about each track. Create and save
-            new playlists of recommended tracks based on your existing playlists and more.`,
+  description: `A developer-focused web platform providing comprehensive documentation for Remita APIs. Developers can explore detailed guides, API references, authentication methods, request and response formats, and practical integration examples. The platform offers interactive code snippets, best practices, and troubleshooting resources to streamline the integration of Remita’s payment solutions into various applications.`,
   technology: [
     'Angular',
     'Typescript',
@@ -43,9 +39,7 @@ linkText: 'thepixplore.netlify.app'
     image: blogImg || './assets/images/thepixplore.png',
     url:'https://blog.remita.net/',
     title: 'Blog Page',
-    description: `Web app for visualizing personalized Spotify data. View your top artists, top tracks,
-            recently played tracks, and detailed audio information about each track. Create and save
-            new playlists of recommended tracks based on your existing playlists and more.`,
+    description: `A dedicated blog platform for Remita, featuring insightful articles on fintech trends, payment solutions, API integrations, and industry best practices. Developers, businesses, and financial experts can explore in-depth guides, case studies, and updates on Remita’s products and services. The blog also provides tutorials, use cases, and expert opinions to help users maximize the potential of Remita’s payment ecosystem.`,
     technology: [
       'Angular',
       'Typescript',
@@ -55,4 +49,35 @@ linkText: 'thepixplore.netlify.app'
     company: 'Remita',
     year: 2024,
     linkText: 'blog.remita.net'
-    }]
+    },
+  {
+    image: blogImg || './assets/images/thepixplore.png',
+    url:'http://online.cygnusspin.com/',
+    title: 'Online Bet Platform',
+    description: `A dedicated blog platform for Remita, featuring insightful articles on fintech trends, payment solutions, API integrations, and industry best practices. Developers, businesses, and financial experts can explore in-depth guides, case studies, and updates on Remita’s products and services. The blog also provides tutorials, use cases, and expert opinions to help users maximize the potential of Remita’s payment ecosystem.`,
+    technology: [
+      'Nuxtjs',
+      'Pinia',
+      'PrimeVue',
+      'PrimeFlex'
+    ],
+    company: 'Cygnusspin',
+    year: 2023,
+    linkText: 'online.cygnusspin.com'
+    },
+  {
+    image: blogImg || './assets/images/thepixplore.png',
+    url:'https://rumuomasichurchofchrist.org/',
+    title: 'Rumuomasi Church website',
+    description: `A dedicated blog platform for Remita, featuring insightful articles on fintech trends, payment solutions, API integrations, and industry best practices. Developers, businesses, and financial experts can explore in-depth guides, case studies, and updates on Remita’s products and services. The blog also provides tutorials, use cases, and expert opinions to help users maximize the potential of Remita’s payment ecosystem.`,
+    technology: [
+      'vuejs',
+      'vuex',
+      'Bootstrap',
+      'FireBase'
+    ],
+    company: 'Rumuomasi Church',
+    year: 2023,
+    linkText: 'rumuomasichurchofchrist.org'
+    },
+  ]
